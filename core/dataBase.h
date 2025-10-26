d32d15797019b50de36e3dd01a5f67a9df92c97a
kaiqideng <157016275+kaiqideng@users.noreply.github.com>
2025-10-27 00:07:54 +0100

    Add files via upload

---
+++ core/dataBase.h
@@ -0,0 +1,255 @@
+#pragma once
+#include "integrate.h"
+#include "fileEditingTool.h"
+#include "generatePoints.h"
+
+class dataBase
+{
+public:
+    dataBase()
+    {
+        simPara.maximumTime = 1.;
+        simPara.timeStep = 1.;
+        simPara.numSteps = 1;
+        simPara.iStep = 0;
+        simPara.numFrames = 1;
+        simPara.iFrame = 0;
+        simPara.frameInterval = 1;
+        gpuPara.deviceIndex = 0;
+        gpuPara.maxThreadsPerBlock = 256;
+        domainOrigin = make_double3(0, 0, 0);
+        domainSize = make_double3(1, 1, 1);
+        gravity = make_double3(0, 0, -9.81);
+        fluidIntegrateGap = 1;
+    }
+
+    ~dataBase()
+    {
+        dev.release();
+    }
+
+protected:
+
+    void setGPUParameterDeviceIndex(int index)
+    {
+        gpuPara.deviceIndex = index;
+    }
+
+    void setSimulationParameterMaximumTime(double t)
+    {
+        if (t <= 0.)
+        {
+			std::cout << "Error: maximum time must be positive." << std::endl;
+			return;
+        }
+        if (simPara.iStep > 0)
+        {
+            std::cout << "Warning: cannot change the number of frames during simulation." << std::endl;
+            return;
+        }
+        simPara.maximumTime = t;
+    }
+
+    void setSimulationParameterNumFrames(int n)
+    {
+		if (n < 1)
+		{
+			std::cout << "Error: number of frames must be at least 1." << std::endl;
+			return;
+		}
+        if (simPara.iStep > 0)
+        {
+			std::cout << "Warning: cannot change the number of frames during simulation." << std::endl;
+			return;
+        }
+        simPara.numFrames = n;
+    }
+
+    void setSimulationParameterTimeStep(double dt)
+    {
+        if (dt <= 0.)
+        {
+			std::cout << "Error: time step must be non-negative." << std::endl;
+			return;
+        }
+        if (simPara.iStep > 0)
+        {
+			std::cout << "Warning: cannot change the time step during simulation." << std::endl;
+            return;
+        }
+		simPara.timeStep = dt;
+    }
+
+    void setFluidIntegrateGap(int gap)
+    {
+        if (gap < 1)
+        {
+            std::cout << "Error: fluid integrate gap must be at least 1." << std::endl;
+            return;
+        }
+		fluidIntegrateGap = gap;
+    }
+
+    void setDomain(double3 origin, double3 size)
+    {
+        domainOrigin = origin;
+        domainSize = size;
+		if (size.x <= 0. || size.y <= 0. || size.z <= 0.)
+		{
+			std::cout << "Error: domain size must be positive in all directions." << std::endl;
+			return;
+		}
+        if (simPara.iStep > 0) setSpatialGrids();
+    }
+
+    void setGravity(double3 g)
+    {
+        gravity = g;
+        if (simPara.iStep > 0) dev.gravity = gravity;
+    }
+
+    void setHertzianContactModel(int mat_i, int mat_j, double E, double G, double res, double k_r_k_s, double k_t_k_s, double mu_s, double mu_r, double mu_t)
+    {
+		int c_ij = hos.contactModels.getCombinedIndex(mat_i, mat_j);
+		if (c_ij < 0)
+		{
+			std::cout << "Error: material index exceeds the number of materials." << std::endl;
+			return;
+		}
+		hos.contactModels.hertzian.E[c_ij] = E;
+		hos.contactModels.hertzian.G[c_ij] = G;
+		hos.contactModels.hertzian.res[c_ij] = res;
+		hos.contactModels.hertzian.k_r_k_s[c_ij] = k_r_k_s;
+		hos.contactModels.hertzian.k_t_k_s[c_ij] = k_t_k_s;
+		hos.contactModels.hertzian.mu_s[c_ij] = mu_s;
+		hos.contactModels.hertzian.mu_r[c_ij] = mu_r;
+		hos.contactModels.hertzian.mu_t[c_ij] = mu_t;
+		if (simPara.iStep > 0) dev.contactModels.copy(hos.contactModels);
+    }
+
+	void setLinearContactModel(int mat_i, int mat_j, double k_n, double k_s, double k_r, double k_t, double d_n, double d_s, double d_r, double d_t, double mu_s, double mu_r, double mu_t)
+	{
+		int c_ij = hos.contactModels.getCombinedIndex(mat_i, mat_j);
+        if (c_ij < 0)
+        {
+            std::cout << "Error: material index exceeds the number of materials." << std::endl;
+            return;
+        }
+		hos.contactModels.linear.k_n[c_ij] = k_n;
+		hos.contactModels.linear.k_s[c_ij] = k_s;
+		hos.contactModels.linear.k_r[c_ij] = k_r;
+		hos.contactModels.linear.k_t[c_ij] = k_t;
+		hos.contactModels.linear.d_n[c_ij] = d_n;
+		hos.contactModels.linear.d_s[c_ij] = d_s;
+		hos.contactModels.linear.d_r[c_ij] = d_r;
+		hos.contactModels.linear.d_t[c_ij] = d_t;
+		hos.contactModels.linear.mu_s[c_ij] = mu_s;
+		hos.contactModels.linear.mu_r[c_ij] = mu_r;
+		hos.contactModels.linear.mu_t[c_ij] = mu_t;
+        if (simPara.iStep > 0) dev.contactModels.copy(hos.contactModels);
+	}
+
+	void setBondedContactModel(int mat_i, int mat_j, double E, double k_n_k_s, double gamma, double sigma_s, double C, double mu)
+	{
+		int c_ij = hos.contactModels.getCombinedIndex(mat_i, mat_j);
+        if (c_ij < 0)
+        {
+            std::cout << "Error: material index exceeds the number of materials." << std::endl;
+            return;
+        }
+		hos.contactModels.bonded.E[c_ij] = E;
+		hos.contactModels.bonded.k_n_k_s[c_ij] = k_n_k_s;
+		hos.contactModels.bonded.gamma[c_ij] = gamma;
+		hos.contactModels.bonded.sigma_s[c_ij] = sigma_s;
+		hos.contactModels.bonded.C[c_ij] = C;
+		hos.contactModels.bonded.mu[c_ij] = mu;
+		if (simPara.iStep > 0) dev.contactModels.copy(hos.contactModels);
+	}
+
+    void addFluid(std::vector<double3> p, double3 velocity, double smoothLength, double density, double soundSpeed, double kinematicViscosity);
+
+    void addSolid(std::vector<double3> p, double3 velocity, double radius, double density, int materialID);
+
+    void addFixedSolid(std::vector<double3> p, double radius, int materialID);
+
+	void addCluster(std::vector<double3> p, std::vector<double> radius, std::vector<double> density, double3 velocity, int materialID);
+
+    void addClump(std::vector<double3> p, std::vector<double> radius, double3 centroidPosition, double3 velocity, double3 angularVelocity, double mass, symMatrix inertiaTensor, int materialID);
+
+    void addExternalForce(int index, double3 force);
+
+    void addGlobalDamping(int index, double C_d);
+
+	double getTime() const
+	{
+		return simPara.iStep * simPara.timeStep;
+	}
+
+	const HostSolid& getHostSolidData()
+	{ 
+        if (simPara.iStep < 1) return hos.solids;
+		dev.solids.uploadState(hos.solids);
+		return hos.solids;
+	}
+
+    const HostFluid& getFluidData()
+    {
+        if (simPara.iStep < 1) return hos.fluids;
+        dev.fluids.uploadState(hos.fluids);
+        return hos.fluids;
+    }
+
+private:
+    struct simulationParameter
+    {
+        double maximumTime;
+        double timeStep;
+        int numSteps;
+        int iStep;
+        int numFrames;
+        int iFrame;;
+        int frameInterval;
+    };
+
+    struct GPUParameter
+    {
+        int deviceIndex;
+        int maxThreadsPerBlock;
+    };
+
+    HostData hos;
+    DeviceData dev;
+    simulationParameter simPara;
+    GPUParameter gpuPara;
+    double3 domainOrigin;
+    double3 domainSize;
+    double3 gravity;
+	int fluidIntegrateGap;
+
+    void addFluidData(const HostFluid f);
+
+    void addSolidData(const HostSolid s);
+
+    void addBondData();
+
+    void setSolidNormal();
+
+    void setSpatialGrids();
+
+    void buildDeviceData();
+
+	friend class solverBase;
+
+    double3 gradWendlandKernel3DForSolidNormal(const double3& rij, double h)
+    {
+        double r = length(rij);
+        if (r < 1.e-10 || r >= 2.0 * h) return make_double3(0, 0, 0);
+        double q = r / h;
+        double sigma = 21.0 / (16.0 * pi() * h * h * h);
+        double term = 1.0 - 0.5 * q;
+        double dW_dq = (-2.0 * pow(term, 3) * (1.0 + 2.0 * q) + 2.0 * pow(term, 4));
+        double dWdr = sigma * dW_dq / h;
+        double factor = dWdr / r;
+        return factor * rij;
+    }
+};
---
+++ core/solverBase.cpp
@@ -0,0 +1,333 @@
+#include "solverBase.h"
+
+void solverBase::solve()
+{
+    initialize();
+    while (simPara.iStep <= simPara.numSteps)
+    {
+        update();
+        simPara.iStep++;
+    }
+}
+
+void solverBase::initialize()
+{
+    std::cout << "Initializing..." << std::endl;
+    hos.contactModels = HostSolidContactModel(10);
+    conditionInitialize();
+
+    std::cout << "Using GPU Device " << gpuPara.deviceIndex << std::endl;
+    cudaError_t cudaStatus;
+    cudaStatus = cudaSetDevice(gpuPara.deviceIndex);
+    if (cudaStatus != cudaSuccess)
+    {
+        std::cout << "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?" << std::endl;
+        exit(1);
+    }
+
+    simPara.numSteps = int((simPara.maximumTime) / simPara.timeStep) + 1;
+    simPara.frameInterval = simPara.numSteps / simPara.numFrames;
+    if (simPara.frameInterval < 1) simPara.frameInterval = 1;
+    simPara.iStep = 0;
+	simPara.iFrame = 0;
+
+    buildDeviceData();
+    neighborSearch(dev, 0, 1, gpuPara.maxThreadsPerBlock);
+    addBondData();
+    setSolidNormal();
+
+    removeVtuFiles(dir);
+    removeDatFiles(dir);
+    outputData();
+
+	std::cout << "Initialization completed." << std::endl;
+    simPara.iStep++;
+}
+
+void solverBase::update()
+{
+    neighborSearch(dev, simPara.iStep, fluidIntegrateGap, gpuPara.maxThreadsPerBlock);
+
+    solidIntegrateBeforeContact(dev, simPara.timeStep, gpuPara.maxThreadsPerBlock);
+
+    fluidIntegrate(dev, simPara.timeStep, simPara.iStep, fluidIntegrateGap, gpuPara.maxThreadsPerBlock);
+
+    calSolidContactAfterFluidIntegrate(dev, simPara.timeStep, gpuPara.maxThreadsPerBlock);
+
+    handleDataAfterCalculateContact();
+
+    solidIntegrateAfterContact(dev, simPara.timeStep, gpuPara.maxThreadsPerBlock);
+
+    if (simPara.iStep % simPara.frameInterval == 0)
+    {
+        simPara.iFrame++;
+        std::cout << "Frame " << simPara.iFrame << " at time " << simPara.iStep * simPara.timeStep << std::endl;
+        outputData();
+    }
+}
+
+void solverBase::outputFluidVTU()
+{
+    if(simPara.iFrame > 0) dev.fluids.uploadState(hos.fluids);
+
+    /* --- make sure "output" dir exists (ignore error if it does) --- */
+    MKDIR(dir.c_str());
+
+    /* --- build file name: output/fluid_XXXX.vtu --- */
+    std::ostringstream fname;
+    fname << dir << "/fluid_" << std::setw(4) << std::setfill('0') << simPara.iFrame << ".vtu";
+
+    std::ofstream out(fname.str().c_str());
+    if (!out) throw std::runtime_error("Cannot open " + fname.str());
+
+    out << std::fixed << std::setprecision(10);      // full double precision
+
+    /* ============ XML HEADER ============ */
+    out << "<?xml version=\"1.0\"?>\n"
+        "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" byte_order=\"LittleEndian\">\n"
+        "  <UnstructuredGrid>\n";
+
+    /* ---- global FieldData: TIME + STEP ---- */
+    out << "    <FieldData>\n"
+        "      <DataArray type=\"Float32\" Name=\"TIME\"  NumberOfTuples=\"1\" format=\"ascii\"> "
+        << simPara.iStep * simPara.timeStep << " </DataArray>\n"
+        "      <DataArray type=\"Int32\"   Name=\"STEP\"  NumberOfTuples=\"1\" format=\"ascii\"> "
+        << simPara.iStep << " </DataArray>\n"
+        "    </FieldData>\n";
+
+    int N = hos.fluids.points.num;
+    /* ---- start Piece ---- */
+    out << "    <Piece NumberOfPoints=\"" << N
+        << "\" NumberOfCells=\"" << N << "\">\n";
+
+    /* ---- Points ---- */
+    out << "      <Points>\n"
+        "        <DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) {
+        const double3& p = hos.fluids.points.position[i];
+        out << ' ' << p.x << ' ' << p.y << ' ' << p.z;
+    }
+    out << "\n        </DataArray>\n"
+        "      </Points>\n";
+
+    /* ---- Cells: one VTK_VERTEX per point ---- */
+    out << "      <Cells>\n"
+        "        <DataArray type=\"Int32\" Name=\"connectivity\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) out << ' ' << i;
+    out << "\n        </DataArray>\n"
+        "        <DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\">\n";
+    for (int i = 1; i <= N; ++i) out << ' ' << i;
+    out << "\n        </DataArray>\n"
+        "        <DataArray type=\"UInt8\" Name=\"types\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) out << " 1";          // 1 = VTK_VERTEX
+    out << "\n        </DataArray>\n"
+        "      </Cells>\n";
+
+    /* ---- PointData ---- */
+    out << "      <PointData Scalars=\"smooth length\">\n";
+
+    /* helper lambdas replaced by small inline fns (C++03 safe) */
+    {   /* scalar double array */
+        out << "        <DataArray type=\"Float32\" Name=\"smooth length\" format=\"ascii\">\n";
+        for (int i = 0; i < N; ++i) {
+            out << ' ' << hos.fluids.points.effectiveRadii[i];
+        }
+        out << "\n        </DataArray>\n";
+    }
+    /* vector<double3> helper */
+    const struct {
+        const char* name;
+        const std::vector<double3>& vec;
+    } vec3s[] = {
+        { "velocity"       , hos.fluids.dyn.velocities        }
+    };
+    for (size_t k = 0; k < sizeof(vec3s) / sizeof(vec3s[0]); ++k) {
+        out << "        <DataArray type=\"Float32\" Name=\"" << vec3s[k].name
+            << "\" NumberOfComponents=\"3\" format=\"ascii\">\n";
+        const std::vector<double3>& v = vec3s[k].vec;
+        for (size_t i = 0; i < v.size(); ++i) {
+            out << ' ' << v[i].x << ' ' << v[i].y << ' ' << v[i].z;
+        }
+        out << "\n        </DataArray>\n";
+    }
+
+    out << "        <DataArray type=\"Float32\" Name=\"density\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) {
+        out << ' ' << hos.fluids.rho0[i] + hos.fluids.dRho[i];
+    }
+    out << "\n        </DataArray>\n";
+
+    out << "        <DataArray type=\"Float32\" Name=\"pressure\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) {
+        out << ' ' << hos.fluids.dRho[i] * pow(hos.fluids.c[i], 2);
+    }
+    out << "\n        </DataArray>\n";
+
+    out << "      </PointData>\n"
+        "    </Piece>\n"
+        "  </UnstructuredGrid>\n"
+        "</VTKFile>\n";
+}
+
+void solverBase::outputSolidVTU()
+{
+    if (simPara.iFrame > 0) dev.solids.uploadState(hos.solids);
+
+    MKDIR(dir.c_str());
+    std::ostringstream fname;
+    fname << dir << "/solid_" << std::setw(4) << std::setfill('0') << simPara.iFrame << ".vtu";
+    std::ofstream out(fname.str().c_str());
+    if (!out) throw std::runtime_error("Cannot open " + fname.str());
+    out << std::fixed << std::setprecision(10);
+    const int N = hos.solids.points.num;
+    out << "<?xml version=\"1.0\"?>\n"
+        "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" byte_order=\"LittleEndian\">\n"
+        "  <UnstructuredGrid>\n";
+
+    out << "    <FieldData>\n"
+        "      <DataArray type=\"Float32\" Name=\"TIME\"  NumberOfTuples=\"1\" format=\"ascii\"> "
+        << simPara.iStep * simPara.timeStep << " </DataArray>\n"
+        "      <DataArray type=\"Int32\"   Name=\"STEP\"  NumberOfTuples=\"1\" format=\"ascii\"> "
+        << simPara.iStep << " </DataArray>\n"
+        "    </FieldData>\n";
+
+    out << "    <Piece NumberOfPoints=\"" << N
+        << "\" NumberOfCells=\"" << N << "\">\n";
+
+    out << "      <Points>\n"
+        "        <DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) {
+        const double3& p = hos.solids.points.position[i];
+        out << ' ' << p.x << ' ' << p.y << ' ' << p.z;
+    }
+    out << "\n        </DataArray>\n"
+        "      </Points>\n";
+
+    out << "      <Cells>\n"
+        "        <DataArray type=\"Int32\" Name=\"connectivity\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) out << ' ' << i;
+    out << "\n        </DataArray>\n"
+        "        <DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\">\n";
+    for (int i = 1; i <= N; ++i) out << ' ' << i;
+    out << "\n        </DataArray>\n"
+        "        <DataArray type=\"UInt8\" Name=\"types\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) out << " 1";          // 1 = VTK_VERTEX
+    out << "\n        </DataArray>\n"
+        "      </Cells>\n";
+
+    out << "      <PointData Scalars=\"radius\">\n";
+
+    out << "        <DataArray type=\"Float32\" Name=\"radius\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) out << ' ' << hos.solids.radius[i];
+    out << "\n        </DataArray>\n";
+
+	//out << "        <DataArray type=\"Float32\" Name=\"pressure\" format=\"ascii\">\n";
+	//for (int i = 0; i < N; ++i) out << ' ' << hos.solids.pressure[i];
+	//out << "\n        </DataArray>\n";
+
+    out << "        <DataArray type=\"Int32\" Name=\"clusterID\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) out << ' ' << hos.solids.clusterID[i];
+    out << "\n        </DataArray>\n";
+
+	out << "        <DataArray type=\"Int32\" Name=\"clumpID\" format=\"ascii\">\n";
+	for (int i = 0; i < N; ++i) out << ' ' << hos.solids.clumpID[i];
+	out << "\n        </DataArray>\n";
+
+    const struct {
+        const char* name;
+        const std::vector<double3>& vec;
+    } vec3s[] = {
+        { "velocity"       , hos.solids.dyn.velocities        },
+		{ "angularVelocity", hos.solids.angularVelocities     },
+		{ "normal"         , hos.solids.normal                }
+    };
+    for (size_t k = 0; k < sizeof(vec3s) / sizeof(vec3s[0]); ++k) {
+        out << "        <DataArray type=\"Float32\" Name=\"" << vec3s[k].name
+            << "\" NumberOfComponents=\"3\" format=\"ascii\">\n";
+        const std::vector<double3>& v = vec3s[k].vec;
+        for (size_t i = 0; i < v.size(); ++i)
+            out << ' ' << v[i].x << ' ' << v[i].y << ' ' << v[i].z;
+        out << "\n        </DataArray>\n";
+    }
+
+    out << "      </PointData>\n"
+        "    </Piece>\n"
+        "  </UnstructuredGrid>\n"
+        "</VTKFile>\n";
+}
+
+void solverBase::outputFluid2SolidVTU()
+{
+    if (simPara.iFrame > 0)
+    {
+        dev.solids.uploadState(hos.solids);
+        dev.fluids.uploadState(hos.fluids);
+        dev.fluid2Solid.upload(hos.fluid2Solid);
+    }
+
+    MKDIR(dir.c_str());
+    std::ostringstream fname;
+    fname << dir << "/fluid2Solid_" << std::setw(4) << std::setfill('0') << simPara.iFrame << ".vtu";
+    std::ofstream out(fname.str().c_str());
+    if (!out) throw std::runtime_error("Cannot open " + fname.str());
+    out << std::fixed << std::setprecision(10);
+    const int N = hos.fluid2Solid.num;
+    out << "<?xml version=\"1.0\"?>\n"
+        "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" byte_order=\"LittleEndian\">\n"
+        "  <UnstructuredGrid>\n";
+
+    out << "    <FieldData>\n"
+        "      <DataArray type=\"Float32\" Name=\"TIME\"  NumberOfTuples=\"1\" format=\"ascii\"> "
+        << simPara.iStep * simPara.timeStep << " </DataArray>\n"
+        "      <DataArray type=\"Int32\"   Name=\"STEP\"  NumberOfTuples=\"1\" format=\"ascii\"> "
+        << simPara.iStep << " </DataArray>\n"
+        "    </FieldData>\n";
+
+    out << "    <Piece NumberOfPoints=\"" << N
+        << "\" NumberOfCells=\"" << N << "\">\n";
+
+    out << "      <Points>\n"
+        "        <DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) {
+        const int& idx_i = hos.fluid2Solid.objectPointed[i];
+        const int& idx_j = hos.fluid2Solid.objectPointing[i];
+        double3 p = 0.5 * (hos.fluids.points.position[idx_i] + hos.solids.points.position[idx_j]);
+        out << ' ' << p.x << ' ' << p.y << ' ' << p.z;
+    }
+    out << "\n        </DataArray>\n"
+        "      </Points>\n";
+
+    out << "      <Cells>\n"
+        "        <DataArray type=\"Int32\" Name=\"connectivity\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) out << ' ' << i;
+    out << "\n        </DataArray>\n"
+        "        <DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\">\n";
+    for (int i = 1; i <= N; ++i) out << ' ' << i;
+    out << "\n        </DataArray>\n"
+        "        <DataArray type=\"UInt8\" Name=\"types\" format=\"ascii\">\n";
+    for (int i = 0; i < N; ++i) out << " 1";          // 1 = VTK_VERTEX
+    out << "\n        </DataArray>\n"
+        "      </Cells>\n";
+
+    out << "      <PointData Scalars=\"force\">\n";
+
+    const struct {
+        const char* name;
+        const std::vector<double3>& vec;
+    } vec3s[] = {
+        { "force"       , hos.fluid2Solid.force        }
+    };
+    for (size_t k = 0; k < sizeof(vec3s) / sizeof(vec3s[0]); ++k) {
+        out << "        <DataArray type=\"Float32\" Name=\"" << vec3s[k].name
+            << "\" NumberOfComponents=\"3\" format=\"ascii\">\n";
+        const std::vector<double3>& v = vec3s[k].vec;
+        for (size_t i = 0; i < v.size(); ++i)
+            out << ' ' << v[i].x << ' ' << v[i].y << ' ' << v[i].z;
+        out << "\n        </DataArray>\n";
+    }
+
+    out << "      </PointData>\n"
+        "    </Piece>\n"
+        "  </UnstructuredGrid>\n"
+        "</VTKFile>\n";
+}
